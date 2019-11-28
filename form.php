df3f6c1b4a79df3adced826b3a656af53be75d2c
Stijn van_der_Made <2120804@talnet.nl>
2019-11-28 10:18:45 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<form action='' method='post'>" . 
+"<input id='POST-name' type='email' placeholder='Voer je email adres in' name='name'>" . 
+"<input type='submit' value='Verstuur'>" . 
+"</form>";
+
+?>
